3d62900674e66d17f40cda9584f32ac340f7fbe3
Julio2-3 acerola <julio72ywy@gmail.com>
2026-08-07 17:44:42 -0300

    features: telas do portal aluno inicio (versao 1.0)

--- apps/portal_aluno/urls.py
+++ apps/portal_aluno/urls.py
@@ -1,9 +1,12 @@
-from django.urls import include, path
+from django.urls import path
 from . import views
 
 
 app_name = 'portal_aluno'
 urlpatterns = [
     path('', views.login, name='login'),
-  
+    path('painel/', views.painel, name='painel'),
+    path('grade-horario/', views.grade_horario, name='grade_horario'),
+    path('perfil/', views.perfil, name='perfil'),
+    path('sair/', views.logout, name='logout'),
 ]
